efb55e29bd86b9e771ec55e5a5c15aa6a98cc493
Narisse-H <juzi-ex@outlook.com>
2025-11-26 19:10:04 +0800

    修bug

--- Ave-Novake/Assets/Scripts/CameraSystem.cs
+++ Ave-Novake/Assets/Scripts/CameraSystem.cs
@@ -6,21 +6,6 @@ public class CameraSystem : MonoBehaviour
 {
     public Transform player_target;
     public float move_time;
-    /*
-    private void LateUpdate()
-    {
-        if (player_target != null && player_target.position != transform.position)
-        {
-            
-            if (player_target.position != transform.position)
-            {
-                transform.position = Vector3.Lerp(transform.position,player_target.position,move_time * Time.deltaTime);
-            }
-            
-            transform.position = Vector3.Lerp(transform.position,player_target.position,move_time * Time.deltaTime);
-        }
-    }
-    */
     void Start()
     {
         player_target = GameObject.FindGameObjectWithTag("Player").transform;
--- Ave-Novake/Assets/Scripts/PlayerAction.cs
+++ Ave-Novake/Assets/Scripts/PlayerAction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    //移动相关
     public float player_speed;
     public float player_jump_speed;
     public bool player_two_feet_on_the_ground;
@@ -19,6 +20,7 @@ public class NewBehaviourScript : MonoBehaviour
         player_rb = GetComponent<Rigidbody2D>();
         player_coll = GetComponent<Collider2D>();
         player_anim = GetComponent<Animator>();
+        player_feet = GameObject.FindGameObjectWithTag("Feet").transform;
     }
 
     // Update is called once per frame
@@ -29,19 +31,29 @@ public class NewBehaviourScript : MonoBehaviour
         PlayerMove();
     }
 
+    //移动（运动）函数
     void PlayerMove()
     {
+        //检测水平方向按键是否被按下
         float horizontal_num = Input.GetAxis("Horizontal");
         float face = Input.GetAxisRaw("Horizontal");
+
+        //判断朝向
         if (face != 0)
         {
             transform.localScale = new Vector3(-face, transform.localScale.y, transform.localScale.z);
         }
+
+        //改变水平速度
         player_rb.velocity = new Vector2(player_speed * horizontal_num, player_rb.velocity.y);
+
+        //改变竖直速度
         if (Input.GetButton("Jump") && player_two_feet_on_the_ground)
         {
             player_rb.velocity = new Vector2(player_rb.velocity.x,player_jump_speed);
         }
+
+        //防二段跳
         if (player_two_feet_on_the_ground)
         {
             player_anim.SetFloat("run",Mathf.Abs(player_speed * horizontal_num));
@@ -53,7 +65,8 @@ public class NewBehaviourScript : MonoBehaviour
             player_anim.SetBool("jump",true);
         }
     }
-    
+
+    //攻击系统
     void PlayerAttack()
     {
         if (Input.GetButtonDown("Fire1"))
