cf48c46dd693f6f16cfef6686044d60fb9e9f37f
Narisse-H <juzi-ex@outlook.com>
2025-11-29 18:02:45 +0800

    场景适配中

--- Ave-Novake/Assets/Scripts/CameraSystem.cs
+++ Ave-Novake/Assets/Scripts/CameraSystem.cs
@@ -12,15 +12,24 @@ public class CameraSystem : MonoBehaviour
     }
     void Update()
     {
-        if (player_target != null && player_target.position != transform.position)
+        CameraMoving(-5.0f, 40.0f);
+    }
+
+    void CameraMoving(float left_b, float right_b)
+    {
+        Vector3 target_position = transform.position;
+        if (player_target != null && player_target.position != transform.position && player_target.position.x > left_b && player_target.position.x < right_b)
         {
-            
-            if (player_target.position != transform.position)
-            {
-                transform.position = Vector3.Lerp(transform.position,player_target.position,move_time * Time.deltaTime);
-            }
-            
-            transform.position = Vector3.Lerp(transform.position,player_target.position,move_time * Time.deltaTime);
+            target_position = player_target.position;
         }
+        else if (player_target.position.x < left_b)
+        {
+            target_position = new Vector3(left_b, player_target.position.y, transform.position.z);
+        }
+        else if (player_target.position.x > right_b)
+        {
+            target_position = new Vector3(right_b, player_target.position.y, transform.position.z);
+        }
+        transform.position = Vector3.Lerp(transform.position, target_position, move_time * Time.deltaTime);
     }
 }
